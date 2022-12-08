a9bc67959e4902efddb4bd551a34e25cf175eaa2
adeelahmed057 <adeelnazir2018@gmail.com>
2022-12-08 18:50:09 +0500

    navbar and footer component ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Pages/Home/Navbar';
+import Footer from './Pages/Footer/Footer';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Footer/>
     </>
   );
 }
---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Footer.css";
+import Footer_Pic from "../../Assets/Images/footer_Pic.png";
+
+function Footer() {
+  return (
+    <div className="footer_bg">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="row mb-4">
+              <div className="col-3 jusitfy-content-center">
+                <div className="">
+                  <img src={Footer_Pic} className="img-fluid" />
+                  {/* <div className="text-left footer-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. 
+                </div> */}
+                </div>
+                <div className="footer-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut.
+                </div>
+              </div>
+              <div className="col-3 text-about">
+                <ul className="mt-3">
+                  <h3 className="text-about-h3">About</h3>
+                  <li className="p-1">CONTACT</li>
+                  <li className="p-1">Brand</li>
+                  <li className="p-1">Blog</li>
+                  <li className="p-1">Community</li>
+                  <li className="p-1">Litepaper</li>
+                  <li className="p-1">Online Store</li>
+                </ul>
+              </div>
+              <div className="col-3 text-about">
+                <ul className="mt-3">
+                  <h3 className="text-about-h3">Help</h3>
+                  <li className="p-1">Customer</li>
+                  <li className="p-1"> Support</li>
+                  <li className="p-1">Troubleshooting</li>
+                  <li className="p-1">Guides</li>
+                </ul>
+              </div>
+              <div className="col-3 text-about">
+                <ul className="mt-3">
+                  <h3 className="text-about-h3">dEVELOPERS</h3>
+                  <li className="p-1">Github</li>
+                  <li className="p-1">Documentation</li>
+                  <li className="p-1">Bug Bounty</li>
+                  <li className="p-1">aUDITS</li>
+                  <li className="p-1">CAREERS</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div className="text-center mb-3 text-copyright">
+          Copyright © 2022 Company Name S.L. All rights reserved.
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
