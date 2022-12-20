817a8dc729c2e163e5630160fa7b1854e3db03a4
adeelahmed057 <adeelnazir2018@gmail.com>
2022-12-20 17:40:00 +0500

    pottery component ui

--- src/components/win/pottery/Pottery.js
+++ src/components/win/pottery/Pottery.js
@@ -8,12 +8,38 @@ function Pottery() {
     <div className='container'>
     <div className='row'>
     <div className='col-md-12 mt-5 mb-5' >
-<div className='row'>
+<div className='row d-flex justify-content-around'>
 <div className='col-md-6 border border-primary'>
+<div className='tp-heading'>
+<div className='tp-vritual'>
+The VirtualSwap Pottery
+</div>
+</div>
+<div className='tp-dollar'>
+$18,166
+</div>
+<div className='tp-to'>
+To be won !
+</div>
+<div>
+    <button className='btn-stake-to'>
+    Stake to Win
+    </button>
+</div>
 
 </div>
-<div className='col-md-6 border border-primary'>
-
+<div className='col-md-6 pottery-box'>
+<div className='p-5  tp-deposit'>
+Deposit VS For <span className='tp-blue'>10 weeks </span>
+to earn <span className='tp-blue'>13% APY </span> and a 
+chance to win prize pot
+</div>
+<div className='text-center tp-time'>
+1D 6H 8M
+</div>
+<div className='text-center tp-until mb-5'>
+Until The Draw    
+</div>
 </div>
 </div>
     </div>
